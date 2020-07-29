882279ea1c5530014a759f21faa1778b4e7aa809
jisaacml <juan.isaac@mercadolibre.com>
2020-07-29 10:29:08 -0300

    list hash - success

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import '../global';
 import Header from './screens/Header';
 import Balance from './screens/Balance';
 import History from './screens/History';
-import { getBalance, getTransactionsByAccount, getTransactionsByAccountEtherScan } from './services/web3'
+import { getBalance, getTransactionsByAccount } from './services/web3'
 import * as Constants from './utils/constants';
 
 
@@ -18,7 +18,6 @@ const App = () => {
   useEffect(() => {
     (async () => {
       await getBalanceByAccount()
-      //await getListHash()
       await getListHashs()
     })();
   }, [])
@@ -29,14 +28,8 @@ const App = () => {
     setSpinnerBalance(false)
   }
 
-  const getListHash = async () => {
-       let response = await getTransactionsByAccount(Constants.ACCOUNT, null, null)
-       setListTransactions(response)
-       setSpinnerHistory(false)
-  } 
-
   const getListHashs = async () => {
-    let response = await getTransactionsByAccountEtherScan(Constants.ACCOUNT, 0, null)
+    let response = await getTransactionsByAccount(Constants.ACCOUNT, 0, null)
     if(response.data.status === "1"){
       setListTransactions(response.data.result)
     } else {
--- src/services/web3.js
+++ src/services/web3.js
@@ -15,46 +15,8 @@ export const getBalance = async (account) => {
     return response;
   }
 
+
 export const getTransactionsByAccount = async (account, startBlockNumber, endBlockNumber) => {
-    const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/55e0252523354d0b8c735251a6cea9df"))
-    
-    if (endBlockNumber === null) {
-        await web3.eth.getBlockNumber((err, result) => {
-            if( err !== null || err !== undefined){
-                endBlockNumber = result
-            }
-        })
-    }
-
-    if (startBlockNumber === null) {
-      startBlockNumber = endBlockNumber - 5000;
-    }
-
-    let transactionsHash = []
-    let transactionsBlock = []
-    
-    for (let i = startBlockNumber; i <= endBlockNumber; i++) {
-        await web3.eth.getBlock(i, (err, block) => {
-            if( err !== null || err !== undefined){
-              for (const item of block.transactions) {
-                transactionsBlock.push(item)
-              }
-            }
-        })
-    }
-
-    for(const item of transactionsBlock){
-        await web3.eth.getTransaction(item, (err, tx) => {
-            if( (tx !== undefined && tx !== null) && (err !== null || err !== undefined) && (account === "*" || account === tx.from || account === tx.to)) {
-                transactionsHash.push(tx);
-            }
-        })
-    }
-
-    return transactionsHash;
-}
-
-export const getTransactionsByAccountEtherScan = async (account, startBlockNumber, endBlockNumber) => {
     try {
         const web3 = new Web3(new Web3.providers.HttpProvider(Constants.URL_INFURA))
         if (endBlockNumber === null) {
